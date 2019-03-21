e2bccbcc71a62c2e29dea4608b8d093d747094f8
juansantane <juan.santa@nebulae.com.co>
2019-03-21 18:28:10 -0500

    force deployment

--- backend/client/bin/server.js
+++ backend/client/bin/server.js
@@ -13,7 +13,6 @@ const KeycloakDA = require('./data/KeycloakDA').singleton();
 const graphQlService = require('./services/emi-gateway/GraphQlService')();
 const Rx = require('rxjs');
 
-
 const start = () => {
     Rx.concat(
         eventSourcing.eventStore.start$(),
